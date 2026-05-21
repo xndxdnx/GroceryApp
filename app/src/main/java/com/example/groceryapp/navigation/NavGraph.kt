14a1a1ca8207f6973829d604a39874102f8cd032
Vlad <xndxdnx@gmail.com>
2026-05-21 11:13:02 +0300

    Главный экран начал делать

--- app/src/main/java/com/example/groceryapp/navigation/NavGraph.kt
+++ app/src/main/java/com/example/groceryapp/navigation/NavGraph.kt
@@ -1,17 +1,19 @@
 package com.example.groceryapp.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
-import com.example.groceryapp.R
+import com.example.groceryapp.main.data.MainHost
 import com.example.groceryapp.preview_splash_screens.screens.FirstSplashScreen
 import com.example.groceryapp.preview_splash_screens.screens.SecondSplashScreen
 import com.example.groceryapp.preview_splash_screens.screens.ThirdSplashScreen
 import com.example.groceryapp.welcome_login_singup_screens.screens.signup_screen.SignUpScreen
 import com.example.groceryapp.welcome_login_singup_screens.screens.login_screen.LoginScreen
 import com.example.groceryapp.welcome_login_singup_screens.screens.welcome_screen.WelcomeScreen
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.serialization.Serializable
 
 
@@ -24,26 +26,6 @@ object AuthGraph
 
 @Serializable
 object MainScreenRoute // Сам экран с Bottom Bar
-
-
-@Serializable
-object MainGraph            // Граф главного экрана
-
-// Цели для вкладок Bottom Bar
-@Serializable object CatalogRoute
-@Serializable object CartRoute
-@Serializable object ProfileRoute
-@Serializable object FavoriteRoute
-
-sealed class BottomBarItem<T : Any>(
-    val route: T,
-    val iconRes: Int
-) {
-    object Catalog : BottomBarItem<CatalogRoute>(CatalogRoute, R.drawable.catalog_ic)
-    object Cart : BottomBarItem<CartRoute>(CartRoute, R.drawable.cart_icon)
-    object Profile : BottomBarItem<ProfileRoute>(ProfileRoute, R.drawable.create_an_account_logo)
-    object Favorite : BottomBarItem<FavoriteRoute>(FavoriteRoute, R.drawable.favorite_icon)
-}
 
 @Serializable
 object FirstSplashRoute
@@ -66,23 +48,27 @@ object LoginScreenRoute
 @Composable
 fun NavGraph() {
 
-    val navController = rememberNavController()
+    val globalNavController = rememberNavController()
+
+    val calculatedStartDestination = remember {
+        if (FirebaseAuth.getInstance().currentUser != null) MainScreenRoute else SplashGraph
+    }
 
     NavHost(
-        navController = navController,
-        startDestination = SplashGraph
+        navController = globalNavController,
+        startDestination = calculatedStartDestination
 
     ) {
         // Граф сплеш-экранов
         navigation<SplashGraph>(startDestination = FirstSplashRoute) {
             composable<FirstSplashRoute> {
                 FirstSplashScreen(onButtonClick = {
-                    navController.navigate(SecondSplashRoute)
+                    globalNavController.navigate(SecondSplashRoute)
                 })
             }
             composable<SecondSplashRoute> {
                 SecondSplashScreen(onButtonClick = {
-                    navController.navigate(ThirdSplashRoute) {
+                    globalNavController.navigate(ThirdSplashRoute) {
                         popUpTo(FirstSplashRoute)
                         popUpTo(SecondSplashRoute)
                     }
@@ -91,12 +77,12 @@ fun NavGraph() {
             composable<ThirdSplashRoute> {
                 ThirdSplashScreen(
                     onButtonClick = {
-                        navController.navigate(AuthGraph) {
+                        globalNavController.navigate(AuthGraph) {
 
                         }
                     },
                     onButtonScipClick = {
-                        navController.navigate(AuthGraph) {
+                        globalNavController.navigate(AuthGraph) {
 
                         }
                     }
@@ -108,40 +94,55 @@ fun NavGraph() {
             composable<WelcomeScreenRoute> {
                 WelcomeScreen(
                     loginClick = {
-                        navController.navigate(LoginScreenRoute) {
+                        globalNavController.navigate(LoginScreenRoute) {
                             launchSingleTop = true
                         }
                     },
                     createAccountClick = {
-                        navController.navigate(SignUpScreenRoute) {
+                        globalNavController.navigate(SignUpScreenRoute) {
                             launchSingleTop = true
                         }
                     },
-                    backClick = { navController.popBackStack() }
+                    backClick = { globalNavController.popBackStack() }
                 )
             }
             composable<SignUpScreenRoute> {
                 SignUpScreen(
                     loginClick = {
-                        navController.navigate(LoginScreenRoute) {
+                        globalNavController.navigate(LoginScreenRoute) {
                             popUpTo(WelcomeScreenRoute) { inclusive = false }
                             launchSingleTop = true
                         }
                     },
-                    backClick = { navController.popBackStack() }
+                    backClick = { globalNavController.popBackStack() },
+                    onSignUpSuccess = {
+                        globalNavController.navigate(MainScreenRoute) {
+                            popUpTo(AuthGraph) { inclusive = true }
+                        }
+                    }
+
                 )
             }
             composable<LoginScreenRoute> {
                 LoginScreen(
                     signUpClick = {
-                        navController.navigate(SignUpScreenRoute) {
+                        globalNavController.navigate(SignUpScreenRoute) {
                             popUpTo(WelcomeScreenRoute) { inclusive = false }
                             launchSingleTop = true
                         }
                     },
-                    backClick = { navController.popBackStack() }
+                    backClick = { globalNavController.popBackStack() },
+                    onSignInSuccess = {
+                        globalNavController.navigate(MainScreenRoute) {
+                            popUpTo(AuthGraph) { inclusive = true }
+                        }
+                    }
                 )
             }
+
+        }
+        composable<MainScreenRoute> {
+            MainHost() // Вызываем экран с его внутренним Scaffold и навигатором
         }
     }
 }
